befdd692b030470c4faad7d7d250b3fcfafe8d45
Sega <igor_segin@meta.ua>
2020-08-03 19:45:52 +0300

    Updates

---
+++ precache-manifest.cf6a3a28078eb2fae81f921269811fdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c483cd2a2a0f40022889bc97c266bbe",
+    "url": "/React-calculator/index.html"
+  },
+  {
+    "revision": "95f61b22600b8e11f6ff",
+    "url": "/React-calculator/static/css/main.4939e5a8.chunk.css"
+  },
+  {
+    "revision": "00d634475185ce161b7f",
+    "url": "/React-calculator/static/js/2.48461748.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/React-calculator/static/js/2.48461748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f61b22600b8e11f6ff",
+    "url": "/React-calculator/static/js/main.26181b92.chunk.js"
+  },
+  {
+    "revision": "cb12bc346802b5c7ad3a",
+    "url": "/React-calculator/static/js/runtime-main.36493dcb.js"
+  }
+]);
